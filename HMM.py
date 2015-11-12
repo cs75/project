91a5ba2301206fbad3f515d8f38e9bfdcf0254c8
cli <charles.dt.li@gmail.com>
2015-11-11 22:25:56 -0500

    refactoring hmm

--- HMM.py
+++ HMM.py
@@ -1,20 +1,47 @@
 
-        
+
 from HMMModel import *
 from State import *
 import math
 
 NUM_STATES = 3
 
+
+
+class Cell(object):
+    """
+    Represents a cell in the matrix
+    
+    row
+    col
+    state = "mat" "del" "ins"
+    back = cell we came from (best cell)
+    """
+
+    def __init__(self, row, col, value):
+        self.row = row
+        self.col = col
+        self.state = None
+        self.back = None
+        self.value = value
+
+    def __str__(self):
+        return "row: ", self.row, "col: ", self.col, "state: ", self.state, "back row: ", self.back.row, "back col: ", self.back.col, "value: ", self.value
+
+    def __repr__(self):
+        return str(self.value)
+
+
+
 def Align(data):
     '''Description
-    '''    
+    '''
     training_data = open(data, 'r')
     aligned_sequences = []
-    
+
     curr_seq = None
     curr_line = training_data.readline()
-    
+
     while curr_line != '': # populate aligned_sequences from training_data
         if curr_line[0] != '>':
             curr_seq += curr_line[:-1] # remove newline character
@@ -24,11 +51,11 @@ def Align(data):
         else:
             curr_seq = ''
         curr_line = training_data.readline()
-    
+
     training_data.close()
-    
+
     return aligned_sequences
-    
+
 # aligned_sequences = Align('training_data.txt') # test
 aligned_sequences = [   'AG---C',
                         'A-AG-C',
@@ -36,19 +63,19 @@ aligned_sequences = [   'AG---C',
                         '--AAAC',
                         'AG---C'
                     ]
-                    
+
 def count_symbols(string, symbols, psuedocount=float(0)):
     symbol_counts = dict.fromkeys(symbols,psuedocount)
     for symbol in string:
         symbol_counts[symbol] += 1
     return symbol_counts
-    
+
 
 def build_model(aligned_sequences, psuedo_count=1, nucleotides=True):
     '''Description
     '''
     alphabet = 'ACGT' if nucleotides == True else 'GPAVLIMCFYWHKRQNEDST'
-    
+
     states = []
     match_state_count = 3 # change to 0 after check
     for pos in range(len(aligned_sequences[0])):
@@ -59,10 +86,10 @@ def build_model(aligned_sequences, psuedo_count=1, nucleotides=True):
         states.append(state)
         # if state.is_match_state():
             # match_state_count += 1
-    # print 'STATES', ':', states, '\n'
-    
+    # #print 'STATES', ':', states, '\n'
+
     model = HMMModel(alphabet, match_state_count, psuedo_count)
-    
+
     prev_mat_states = []
     prev_ins_states = ''
     for i in range(len(states)):
@@ -71,17 +98,17 @@ def build_model(aligned_sequences, psuedo_count=1, nucleotides=True):
             for emission in states[i]: # update mat emissions
                 if emission != '-':
                     model['mat'][emission][len(prev_mat_states)+1] += 1
-            
+
             for emission in prev_ins_states: # update ins emissions
                 if emission != '-':
                     model['ins'][emission][len(prev_mat_states)] += 1
-                    
+
             if len(prev_mat_states) > 0: # update state transitions
                 for j in range(len(states[i])):
                     e1 = '%s' % states[prev_mat_states[-1]][j]
                     e2 = '%s' % states[i][j]
-                    # print e1, '->', e2
-                    
+                    # #print e1, '->', e2
+
                     if e1 != '-' and e2 != '-':
                         model[('mat','mat')][prev_mat_states[-1]+1] += 1
                     elif e1 != '-' and e2 == '-':
@@ -90,26 +117,20 @@ def build_model(aligned_sequences, psuedo_count=1, nucleotides=True):
                         model[('del','mat')][prev_mat_states[-1]] += 1
                     elif False:
                         model[('del','del')][prev_mat_states[-1]] += 1
-                    
-                # print 
-                
-                
-                
-                
-            
+
             # cleanup
             prev_mat_states.append(i)
             prev_ins_states = ''
-            
+
         else: # insert state
             prev_ins_states += states[i].get_emissions() # keep track until next match state
-    
+
     return model
-    
-    
-    
+
+
+
 # model = build_model(aligned_sequences,0) # test
-# model.print_model()
+# model.#print_model()
 
 
 
@@ -142,7 +163,7 @@ M[('del','mat')][3] += 1
 M[('del','del')][1] += 1
 M[('del','ins')][2] += 2
 
-# M.print_model()
+# M.#print_model()
 
 
 
@@ -156,67 +177,67 @@ NUM_STATES = 3
 
 def viterbi(model,sequence):
     model.normalize()
-    matrix = [[float(0) for col in range(len(sequence)+1)] for row in range(len(model)*3+2)] # matrix
-    
-    for row in range(len(matrix)):
-        print matrix[row]
+    matrix = [[Cell(0, 0, float(0)) for col in range(len(sequence)+1)] for row in range(len(model)*3+2)] # matrix
+
+    for row in matrix:
+        print row
+
+    return
+
+
+
+    best = []
+
+    # for row in range(len(matrix)):
+        #print matrix[row]
 
     curr_index = 1
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
             if row > 0 and col > 0 and row < len(model)*3+1:
-                print row,col, '\n', '---'
+                #print row,col, '\n', '---'
                 state = (row - 1) % NUM_STATES + 1
-                print state
-                
-                
+                #print state
+
+
                 if row < 4 and col < 4: # from begin state
                     pass
                 elif state == 1: # match state
-                    #need to fill the matrix now 
-                    print len(model['mat']), "trying to acces: ", col - 1
-                    print len(model['mat'][sequence[col - 1]]), " trying to access row: ", row
-                    
-                    print model['mat'][sequence[col-1]]
-                    print curr_index
-                    print model['mat'][sequence[col-1]][1]
-                    
-                    print curr_index
+                    #need to fill the matrix now
                     emission_prob = math.log(model['mat'][sequence[col-1]][curr_index])
-                    from_mat = matrix[row-3][col-1] + math.log(model[('mat','mat')][curr_index-1])
-                    from_ins = matrix[row-2][col-1] + math.log(model[('ins','mat')][curr_index-1])
-                    print model[('del','mat')][curr_index]
-                    
-                        
+                    from_mat = (matrix[row-3][col-1] + math.log(model[('mat','mat')][curr_index-1]), (row -3, col - 1))
+                    from_ins = (matrix[row-2][col-1] + math.log(model[('ins','mat')][curr_index-1]), (row - 2, col - 1))
+                    #print model[('del','mat')][curr_index]
+
+
                     if curr_index == 1:
                         from_del = 0
                     else:
                         from_del = matrix[row-1][col-1] + math.log(model[('del','mat')][curr_index-1])
-                        
+
                     trans_prob = max(from_mat, from_ins, from_del)
+
+
+
                     matrix[row][col] = emission_prob + trans_prob
-                    
+
                 elif state == 2: # insert state
-                    print model['ins'][sequence[col-1]]
-                    print row
+                    #print model['ins'][sequence[col-1]]
+                    #print row
                     emission_prob = math.log(model['ins'][sequence[col-1]][curr_index])
                     from_mat = matrix[row-1][col-1] + math.log(model[('mat','ins')][curr_index-1])
                     from_ins = matrix[row][col-1] + math.log(model[('ins','ins')][curr_index-1])
-                    
+
                     if curr_index == 1:
                         from_del = 0
                     else:
-                        print 
-                        print
-                        print
-                        print       
                         from_del = matrix[row+1][col-1] + math.log(model[('del','ins')][curr_index-1])
-                    
-                    print 'hererrerererererer', model[('del','ins')][curr_index-1]      
-                    
-                    trans_prob = max(from_mat, from_ins, from_del)                   
+
+                    #print 'hererrerererererer', model[('del','ins')][curr_index-1]
+
+                    trans_prob = max(from_mat, from_ins, from_del)
                     matrix[row][col] = emission_prob + trans_prob
-                    
+
                 elif state == 3: # delete state
                     if curr_index > 1:
                         from_mat = matrix[row-5][col] + math.log(model[('mat','del')][curr_index-1])
@@ -226,11 +247,11 @@ def viterbi(model,sequence):
                         matrix[row][col] = trans_prob
                     if col == len(sequence)-1:
                         curr_index += 1
-                        print "incrementation", curr_index
+                        #print "incrementation", curr_index
             else: # to end state
                 pass
-                
-            print
+
+            #print
     for row in matrix:
         print row
 
@@ -239,4 +260,6 @@ def viterbi(model,sequence):
 
 
 viterbi(M,"ACTGAT")
-#viterbi(M,"TAGATTG")
+viterbi(M,"TAGATTG")
+
+
--- HMMModel.py
+++ HMMModel.py
@@ -9,7 +9,7 @@ class HMMModel:
         width = match_state_count + 1 # accounts for position 0
         every_state = ['mat','ins']
         every_trans = [('mat','mat'),('mat','ins'),('mat','del'),('ins','mat'),('ins','ins'),('ins','del'),('del','mat'),('del','ins'),('del','del')]
-        
+
         self.state = dict.fromkeys(set(every_state))
         for key in every_state:
             self.state[key] = dict.fromkeys(self.alphabet)
@@ -19,24 +19,24 @@ class HMMModel:
                     self.state[key][letter] = [None] + [float(psuedocount) for i in range(width-1)]
                 else:
                     self.state[key][letter] = [float(psuedocount) for i in range(width)]
-        
+
         self.trans = dict.fromkeys(set(every_trans))
         for key in every_trans:
             if key[0] == 'del':
                 self.trans[key] = [None] + [float(psuedocount) for i in range(width-1)]
             else:
                 self.trans[key] = [float(psuedocount) for i in range(width)]
-        
+
     def __getitem__(self,item):
-        if self.state.has_key(item): 
+        if self.state.has_key(item):
             return self.state[item]
-        elif self.trans.has_key(item): 
+        elif self.trans.has_key(item):
             return self.trans[item]
         return None
-        
+
     def __len__(self):
         return self.match_state_count
-        
+
     def normalize(self):
 
         letters = ['A', 'C', 'G', 'T']
@@ -44,12 +44,12 @@ class HMMModel:
         #for each key in the current dictionary
         for key in self.state:
 
-            #for i in the length of the number of states 
+            #for i in the length of the number of states
             for i in range(len(self.state[key][letters[0]])):
 
-                total_count = float(0)        
+                total_count = float(0)
 
-                #for each character in the amino acid alphabet 
+                #for each character in the amino acid alphabet
                 for letter in letters:
 
                     if self.state[key][letter][i] != None:
@@ -61,14 +61,14 @@ class HMMModel:
                         self.state[key][letter][i] /= total_count
                     # print "after normalizing: ", self.state[key][letter][i]
 
-            
+
             init_trans = self.trans.keys()[0]
 
 
             for i in range(len(self.trans[init_trans])):
 
                 total_count = float(0)
-                
+
                 for key in self.trans:
                     if self.trans[key][i] != None:
                         total_count += self.trans[key][i]
@@ -78,10 +78,10 @@ class HMMModel:
                 for key in self.trans:
                     if self.trans[key][i] != None:
                         self.trans[key][i] /= total_count
-                        print "normalized: ", self.trans[key][i]
+                        # print "normalized: ", self.trans[key][i]
 
                 # sleep(1)
-                
+
     def print_model(self):
         print 'MAT Emissions'
         for letter in self.state['mat']:
